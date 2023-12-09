3bc540cbe54935feb351cb1c8064d2b9a72ac405
devusmaarn <devusmaarn@gmail.com>
2023-12-09 07:58:09 +0100

    addelivewire styles and scriptsd

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
+// import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+// import Clipboard from '@ryangjchandler/alpine-clipboard'
 import './bootstrap';
+ 
+// Alpine.plugin(Clipboard)
+ 
+// Livewire.start()
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Agbagbata' }}</title>
-        @vite('resources/css/app.css')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
          @livewireStyles
     </head>
     <body>
@@ -14,7 +14,7 @@
             {{ $slot }}
         </main>
         <x-layouts.footer />
-        
-        @livewireScripts
+
+      @livewireScriptConfig
     </body>
 </html>
